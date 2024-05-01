d286fdfd4b05cff97cf6a813e796b4f2c4042b2b
leduybao2908 <160259375+leduybao2908@users.noreply.github.com>
2024-05-01 09:08:32 +0700

    Add files via upload
    add JDBC code

---
+++ JDBCUtil.java
@@ -0,0 +1,50 @@
+
+package database;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import com.mysql.cj.jdbc.Driver;
+
+public class JDBCUtil {
+	public static Connection getConnection() {
+		Connection c = null;
+		
+		try {
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			
+			String url = "jdbc:mySQL://localhost:3306/badminton_shop";
+			String username = "root";
+			String password = "29082005";
+			
+			// Tạo kết nối
+			c = DriverManager.getConnection(url, username, password);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return c;
+	}
+	public static void closeConnection(Connection c) {
+		try {
+			if(c!=null) {
+				c.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}	
+	public static void printInfo(Connection c) {
+		try {
+			if(c!=null) {
+				DatabaseMetaData mtdt = c.getMetaData();
+				System.out.println(mtdt.getDatabaseProductName());
+				System.out.println(mtdt.getDatabaseProductVersion());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
